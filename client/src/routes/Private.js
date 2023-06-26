1b34d7fb0b355dc90b374d09362f46d47c9ea192
SudevaHarsha <sudevaharsha1@gmail.com>
2023-06-26 11:17:58 +0530

    completed

--- client/src/routes/Private.js
+++ client/src/routes/Private.js
@@ -1,8 +1,8 @@
 import { useState,useEffect } from "react";
-import { useAuth } from "../context/Auth.js";
+import { useAuth } from "../../context/Auth.js";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
-import Spinner from "../components/spinner.js";
+import Spinner from "../Spinner.js";
 
 export default function PrivateRoute(){
     const [ok,setOk]= useState(false);
